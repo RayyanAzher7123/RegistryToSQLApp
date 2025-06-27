ff364f3abfed49346e0281573a06f7e0e21fcdc2
Rayyan Azher Miswani <rayyanazher25@gmail.com>
2025-06-27 08:38:12 -0400

    Initial commit: Satisfactory functionality

---
+++ RegistryToSQLApp/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Data.SqlClient;
+using Microsoft.Win32;
+
+namespace RegistryToSQLApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string registryPath = @"SOFTWARE\WOW6432Node\nexRIS\agora\nexris\Parameters\Business Rules";
+            string registrySection = "Registry";
+            string connString = "Server=localhost;Database=ApplicationConfiguration;Trusted_Connection=True;";
+
+            try
+            {
+                using (RegistryKey baseKey = Registry.LocalMachine.OpenSubKey(registryPath))
+                {
+                    if (baseKey == null)
+                    {
+                        Console.WriteLine("Registry path not found.");
+                        return;
+                    }
+
+                    foreach (var valueName in baseKey.GetValueNames())
+                    {
+                        object value = baseKey.GetValue(valueName);
+                        string valueStr = value != null ? value.ToString() : "MISSING";
+
+                        InsertIntoDatabase(connString, valueName, valueStr, registryPath, registrySection);
+                        Console.WriteLine($"Inserted: {valueName} = {valueStr}");
+                    }
+
+                    Console.WriteLine("All registry values inserted successfully.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR: " + ex.Message);
+            }
+        }
+
+        static void InsertIntoDatabase(string connectionString, string key, string value, string regPath, string section)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                string insertQuery = @"
+    IF NOT EXISTS (
+        SELECT 1 FROM ApplicationConfigurationValues 
+        WHERE ApplicationId = @ApplicationId AND ConfigKey = @ConfigKey AND RegistryNode = @RegistryNode
+    )
+    BEGIN
+        INSERT INTO ApplicationConfigurationValues 
+        (ApplicationId, ConfigKey, ConfigSection, ConfigValue, RegistryNode, RegistryKey) 
+        VALUES (@ApplicationId, @ConfigKey, @ConfigSection, @ConfigValue, @RegistryNode, @RegistryKey)
+    END";
+
+
+                using (SqlCommand cmd = new SqlCommand(insertQuery, conn))
+                {
+                    cmd.Parameters.AddWithValue("@ApplicationId", 1);
+                    cmd.Parameters.AddWithValue("@ConfigKey", key);
+                    cmd.Parameters.AddWithValue("@ConfigSection", section);
+                    cmd.Parameters.AddWithValue("@ConfigValue", value);
+                    cmd.Parameters.AddWithValue("@RegistryNode", regPath);
+                    cmd.Parameters.AddWithValue("@RegistryKey", key);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ RegistryToSQLApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RegistryToSQLApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sobeys Inc.")]
+[assembly: AssemblyProduct("RegistryToSQLApp")]
+[assembly: AssemblyCopyright("Copyright © Sobeys Inc. 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("02735355-ac51-44b4-9cd4-ca943d179048")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
